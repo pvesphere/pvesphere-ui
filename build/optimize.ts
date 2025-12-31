81a07a3f77c335869364885969888a261a3c2bb6
ljohn6530 <ljohn6530@zto.com>
2025-12-31 23:10:06 +0800

    chore: add Apache 2.0 license headers and fix LICENSE format
    - Remove APPENDIX from LICENSE for GitHub recognition
    - Add NOTICE file with copyright information
    - Add Apache 2.0 license headers to all source files
    - Add .licenserc.yaml for license-eye configuration

--- build/optimize.ts
+++ build/optimize.ts
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2025 PveSphere Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /**
  * 此文件作用于 `vite.config.ts` 的 `optimizeDeps.include` 依赖预构建配置项
  * 依赖预构建，`vite` 启动时会将下面 include 里的模块，编译成 esm 格式并缓存到 node_modules/.vite 文件夹，页面加载到对应模块时如果浏览器有缓存就读取浏览器缓存，如果没有会读取本地缓存并按需加载
